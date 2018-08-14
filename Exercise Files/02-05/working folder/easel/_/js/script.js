f449eca35c202fe17522e1de12e2ff153ef8bf50
Adalberto N Garza <adagar@users.noreply.github.com>
2018-08-14 11:44:20 -0400

    Finished section 2

--- Exercise Files/02-05/working folder/easel/_/js/script.js
+++ Exercise Files/02-05/working folder/easel/_/js/script.js
@@ -4,8 +4,18 @@ function init() {
 	shape = new createjs.Shape();
 
 	shape.graphics.beginStroke("purple");
-	shape.graphics.setStrokeStyle(10, 0, 1);
-	
+	shape.graphics.setStrokeStyle(10, 1, 1);
+
+	//shape.graphics.rect(50, 50, 250, 250);
+	//shape.graphics.drawRoundRect(50, 50, 250, 250, 50);
+	//shape.graphics.drawCircle(250, 250, 50);
+	//shape.graphics.drawEllipse(100, 100, 250, 100);
+
+	//shape.graphics.arc(100, 100, 50, 0, 90*(Math.PI/180), 1);
+
+	shape.graphics.drawPolyStar(200, 200, 100, 3, 0.5, -90);
+	//(x, y, radius, numSides, pointyness, angle)
+
 	stage.addChild(shape);
 	stage.update();
 }
--- Exercise Files/02-06/working folder/easel/_/js/script.js
+++ Exercise Files/02-06/working folder/easel/_/js/script.js
@@ -2,13 +2,34 @@ function init() {
 	var canvas = document.getElementById("easel"),
 	stage = new createjs.Stage(canvas),
 	shape = new createjs.Shape();
-	
+
+	//before chaining
+	/*
 	shape.graphics.beginStroke("purple");
 	shape.graphics.setStrokeStyle(10, "round", "round");
 	shape.graphics.moveTo(50, 50);
 	shape.graphics.lineTo(250, 250);
 	shape.graphics.lineTo(50, 250);
 	shape.graphics.closePath();
+	*/
+
+	//after chaining
+	shape.graphics
+		.beginStroke("purple")
+		.setStrokeStyle(10, "round", "round")
+		.moveTo(50, 50)
+		.lineTo(250, 250)
+		.lineTo(50, 250)
+		.closePath();
+
+	//after using shortcuts
+	shape.graphics
+		.s("purple")
+		.ss(10, 1, 1)
+		.mt(250, 250)
+		.lt(50, 350)
+		.lt(250, 350)
+		.cp();
 
 	stage.addChild(shape);
 	stage.update();
