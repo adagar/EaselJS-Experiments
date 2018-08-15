b1b9951ed4a2b19970ab90e66d83310e07cc8cb9
Adalberto N Garza <adagar@users.noreply.github.com>
2018-08-15 15:38:51 -0400

    Completed 3-2

--- Exercise Files/03-01/working folder/easel/_/js/script.js
+++ Exercise Files/03-01/working folder/easel/_/js/script.js
@@ -2,9 +2,34 @@ function init() {
 	var canvas = document.getElementById("easel"),
 	stage = new createjs.Stage(canvas),
 	shape = new createjs.Shape();
-	
-	shape.graphics.f("purple").r(50, 50, 100, 100);
+
+	const STAGEWIDTH = canvas.width;
+	const STAGEHEIGHT = canvas.height;
+	const SHAPEWIDTH = 50;
+	const SHAPEHEIGHT = 100;
+
+	shape.graphics.f("purple").r(0, 0, SHAPEWIDTH, SHAPEHEIGHT);
+	shape.regX = SHAPEWIDTH / 2;
+	shape.regY = SHAPEHEIGHT / 2;
+	shape.x = 200;
+	shape.y = 200;
+	shape.scaleY = 0.75;
+	shape.scaleX = 1.25;
+	shape.skewX = 200;
 
 	stage.addChild(shape);
-	stage.update();
+
+	createjs.Ticker.setFPS(30);
+	var xDir = 5;
+	createjs.Ticker.addListener(function()
+	{
+		//shape.rotation += 8;
+		shape.x += xDir;
+		if(shape.x > STAGEWIDTH || shape.x < 0)
+		{
+			xDir *= -1;
+		}
+		shape.skewX += (4 * xDir);
+		stage.update();
+	});
 }
--- Exercise Files/03-02/working folder/easel/_/js/script.js
+++ Exercise Files/03-02/working folder/easel/_/js/script.js
@@ -1,6 +1,21 @@
 function init() {
-	var canvas = document.getElementById("easel"),
-	stage = new createjs.Stage(canvas);
-	
+	var canvas = document.getElementById("easel");
+	var stage = new createjs.Stage(canvas);
+
+	var sentence = new createjs.Text();
+	sentence.text = "The quick brown fox jumped over the lazy dog.";
+	sentence.font = "bold 30px Times";
+	sentence.color = "blue";
+	sentence.x = 50;
+	sentence.y = 100;
+	sentence.lineWidth = "150";
+
+	var sentence2 = new createjs.Text("The quick brown fox jumped even further", "bold 30px Times", "yellow");
+	sentence2.y = 200;
+	sentence2.rotation = 30;
+	sentence2.textAlign = "center";
+
+	stage.addChild(sentence);
+	stage.addChild(sentence2);
 	stage.update();
 }
