2a170748c3b8f5edca51a8fe6330aa3724beb0a5
Adalberto N Garza <adagar@users.noreply.github.com>
2018-08-16 16:05:07 -0400

    Completed 3-3

--- Exercise Files/03-03/working folder/easel/_/js/script.js
+++ Exercise Files/03-03/working folder/easel/_/js/script.js
@@ -1,10 +1,22 @@
 function init() {
 	var canvas = document.getElementById("easel"),
 	stage = new createjs.Stage(canvas);
-	
+	centerX = canvas.width / 2;
+	centerY = canvas.height / 2;
 
+	var ship = new createjs.Bitmap("images/ship.svg");
+	ship.x = centerX;
+	ship.y = centerY;
+	ship.regX = 50;
+	ship.regY = 50;
+
+
+	stage.addChild(ship);
+	
 	createjs.Ticker.setFPS(30);
 	createjs.Ticker.addListener(function() {
+		ship.rotation += 5;
 		stage.update();
 	});
+	
 }
