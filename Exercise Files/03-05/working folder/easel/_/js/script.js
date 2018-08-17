e90890db572ee42ad82d3574ddd38a5756bb9c9e
Adalberto N Garza <adagar@users.noreply.github.com>
2018-08-17 16:13:02 -0400

    COmpleted section 3-4

--- Exercise Files/03-05/working folder/easel/_/js/script.js
+++ Exercise Files/03-05/working folder/easel/_/js/script.js
@@ -1,10 +1,30 @@
 function init() {
 	var canvas = document.getElementById("easel"),
 	stage = new createjs.Stage(canvas),
-	centerX = canvas.width/2,
-	centerY = canvas.height/2;
-	
-	createjs.Ticker.setFPS(30);
-	createjs.Ticker.addListener(function() {
+	centerX = canvas.width / 2,
+	centerY = canvas.height / 2;
+
+	const ss = new createjs.SpriteSheet({
+		animations: {
+			fly: [0, 15],
+			explode: [16, 20, "fly"] },
+		images: ["images/shipsprites.png"],
+		frames: {
+			regX: 50,
+			regY: 50,
+			width: 100,
+			height: 100
+		}
+	});
+	const ship = new createjs.BitmapAnimation(ss);
+
+	ship.x = centerX;
+	ship.y = centerY;
+	ship.gotoAndPlay("explode");
+
+	stage.addChild(ship);
+	createjs.Ticker.setFPS(10);
+	createjs.Ticker.addListener(function(){
+		stage.update();
 	});
 }
