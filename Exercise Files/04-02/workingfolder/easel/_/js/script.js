f90ba391ede2857c6a2940bfa3441b301cf1baf9
Adalberto N Garza <adagar@users.noreply.github.com>
2018-08-20 14:47:42 -0400

    Completed LiL course

--- Exercise Files/04-02/workingfolder/easel/_/js/script.js
+++ Exercise Files/04-02/workingfolder/easel/_/js/script.js
@@ -1,8 +1,8 @@
-function init() {
+function init(){
 	var canvas = document.getElementById("easel"),
 	stage = new createjs.Stage(canvas),
-	centerX = canvas.width/2,
-	centerY = canvas.height/2,
+	centerX = canvas.width / 2,
+	centerY = canvas.height / 2,
 	ss = new createjs.SpriteSheet({
 		animations: {
 			fly: [0, 15],
@@ -10,19 +10,44 @@ function init() {
 		images: ["images/shipsprites.png"],
 		frames: {
 			regX: 50,
-			regY: 50,
+			regY: 50, 
 			height: 100,
 			width: 100
-		}
+		}		
 	}),
 	ship = new createjs.BitmapAnimation(ss);
 	ship.x = centerX;
 	ship.y = centerY;
 	ship.gotoAndPlay("fly");
+	let gotoX = ship.x;
+	let gotoY = ship.y;
+	const velocity = 10;
 
-	stage.addChild(ship);	
+	ship.onPress = e => {
+		e.onMouseMove = ev => {
+			e.target.x = ev.stageX;
+			e.target.y = ev.stageY;
+		}
+	}
+	
+	/*
+	stage.onClick = evt => {
+		gotoX = evt.stageX;
+		gotoY = evt.stageY;
+		console.log(gotoX, gotoY);
+	}
+	*/
+	stage.enableMouseOver();
+	createjs.MouseEvent
+	stage.addChild(ship);
 	createjs.Ticker.setFPS(30);
-	createjs.Ticker.addListener(function() {
+	createjs.Ticker.addListener(()=>{
+		/*
+		let difX = gotoX - ship.x;
+		let difY = gotoY - ship.y;
+		*/
+		ship.x += difX / velocity;
+		ship.y += difY / velocity;
 		stage.update();
 	});
 }
